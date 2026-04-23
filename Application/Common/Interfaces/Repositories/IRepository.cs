eddcb7f515de549933d7e357e0fd27730ef63505
NRM <roman.nikitchuk123@gmail.com>
2026-04-23 21:08:49 +0200

    feat: add  repository interface

---
+++ Application/Common/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Interfaces;
+
+namespace Application.Common.Interfaces.Repositories;
+
+public interface IRepository<T> where T : class, IEntity
+{
+    Task<T> CreateAsync(T entity, CancellationToken cancellationToken);
+    Task<T> UpdateAsync(T entity, CancellationToken cancellationToken);
+    Task<T> DeleteAsync(T entity, CancellationToken cancellationToken);
+}
