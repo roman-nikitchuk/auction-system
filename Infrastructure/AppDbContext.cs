a59c219f6803e13a885d37f0e3f527a96e0613b8
NRM <roman.nikitchuk123@gmail.com>
2026-04-25 17:20:48 +0200

    feat: add infrastructure structure and AppDbContext

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure;
+
+public class AppDbContext(
+    DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; init; }
+    public DbSet<Auction> Auctions { get; init; }
+    public DbSet<Bid> Bids { get; init; }
+    public DbSet<Category> Categories { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
