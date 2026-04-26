199519ea7907c02c62a4e37f53c0ee641813d51d
NRM <roman.nikitchuk123@gmail.com>
2026-04-26 19:18:33 +0200

    feat: add controllers structure

---
+++ API/Controllers/AuctionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AuctionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ API/Controllers/BidController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BidController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
